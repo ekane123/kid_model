7a7e026d239278cffb0aa488edea79deae516837
ekane123 <ekane@caltech.edu>
2023-03-08 11:38:00 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(name='kid_model',
+      version='0.0',
+      description='Mattis-Bardeen model of KID response and noise',
+      author='Elijah Kane',
+      author_email='ekane@caltech.edu',
+      packages=find_packages(),
+      url="https://github.com/ekane123/kid_model",
+      data_files = [],
+      include_package_data=True,
+      python_requires='>3.8',
+      install_requires=['numpy',
+                        'scipy',
+                        'pandas',
+                        'matplotlib',
+                        #'pyfftw'
+                        #'submm_python_routines @ git+ssh://git@github.com/Wheeler1711/submm_python_routines'
+                        ]
+      )
